5744eb3df005f741c680fdf3e0c22b11ed6bc803
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2025-03-22 22:31:55 -0300

    feat: criando os getters e setters das entidades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -15,6 +15,35 @@ export class Answer extends Entity<AnswerProps> {
     return this.props.content;
   }
 
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get questionId() {
+    return this.props.questionId;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat("...");
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
+  }
+
   static create(
     props: Optional<AnswerProps, "createdAt">,
     id?: UniqueEntityID
