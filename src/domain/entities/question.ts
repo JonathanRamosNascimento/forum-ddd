37cf0af052f585dad60b744d8eb79e39d7f43881
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2025-03-22 22:43:40 -0300

    feat: Path aliases e Vitest globals

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,7 +1,7 @@
 import dayjs from "dayjs";
-import { Entity } from "../../core/entities/entity";
-import { UniqueEntityID } from "../../core/entities/unique-entity-id";
-import { Optional } from "../../core/types/optional";
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Optional } from "@/core/types/optional";
 import { Slug } from "./value-objects/slug";
 
 interface QuestionProps {
--- src/domain/entities/value-objects/slug.spec.ts
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -1,5 +1,4 @@
 import { Slug } from "./slug";
-import { expect, test } from "vitest";
 
 test("it should be able to create a new slug from text", () => {
   const slug = Slug.createFromText("Example question title");
